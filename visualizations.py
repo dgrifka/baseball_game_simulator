a5cdb863675c14501185138f4fc06a6163f77162
Grifka <KS93@molsoncoors.com>
2024-06-04 11:05:35 -0500

    edit visualizations

--- visualizations.py
+++ visualizations.py
@@ -14,8 +14,8 @@ def wp_barplot(num_simulations, home_win_percentage, away_win_percentage, tie_pe
     plt.figure(figsize=(8, 6))
     plt.bar(labels, percentages, color=colors)
     plt.xlabel('Team', fontsize=14)
-    plt.ylabel('Win Percentage', fontsize=14)
-    plt.title(f'Win Percentages ({num_simulations} Simulations)\nActual Score: {away_team} {str(away_score)} - {home_team} {str(home_score)}', fontsize=16)
+    plt.ylabel('Deserve-to-Win', fontsize=14)
+    plt.title(f'Deserve-to-Win ({num_simulations} Simulations)\nActual Score: {away_team} {str(away_score)} - {home_team} {str(home_score)}\nDeserve-to-Win: {away_team} {str(away_win_percentage_str)}% - {home_team} {str(home_win_percentage_str)}%', fontsize=16, pad=12)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
     plt.gca().set_yticklabels([f'{x:.0f}%' for x in plt.gca().get_yticks()])
@@ -24,7 +24,7 @@ def wp_barplot(num_simulations, home_win_percentage, away_win_percentage, tie_pe
     images_dir = "images"
     if not os.path.exists(images_dir):
         os.makedirs(images_dir)
-    plt.savefig(os.path.join(images_dir, f'{away_team}_{home_team}_{str(away_score)}-{str(home_score)}--{str(away_win_percentage_str)}-{str(home_win_percentage_str)}_wp.png'))
+    plt.savefig(os.path.join(images_dir, f'{away_team}_{home_team}_{str(away_score)}-{str(home_score)}--{str(away_win_percentage_str)}-{str(home_win_percentage_str)}_wp.png'), bbox_inches='tight')
     plt.close()
 
 def run_dist(num_simulations, home_runs_scored, away_runs_scored, home_team, away_team, home_score, away_score, home_win_percentage, away_win_percentage):
@@ -32,17 +32,21 @@ def run_dist(num_simulations, home_runs_scored, away_runs_scored, home_team, awa
     home_win_percentage_str = f"{home_win_percentage:.0f}"
     # Graph the distributions of runs scored
     plt.figure(figsize=(10, 6))
-    plt.hist(home_runs_scored, bins=range(max(home_runs_scored)+2), alpha=0.7, label=f'{home_team}', color=team_colors[home_team][0], edgecolor='black', linewidth=1)
-    plt.hist(away_runs_scored, bins=range(max(away_runs_scored)+2), alpha=0.7, label=f'{away_team}', color=team_colors[away_team][0], edgecolor='black', linewidth=1)
+    max_runs = max(max(home_runs_scored), max(away_runs_scored))
+    bins = range(0, max_runs + 2)  # Start from 0 and include the maximum runs scored
+    plt.hist(home_runs_scored, bins=bins, alpha=0.625, label=f'{home_team}', color=team_colors[home_team][0], edgecolor='black', linewidth=1)
+    plt.hist(away_runs_scored, bins=bins, alpha=0.625, label=f'{away_team}', color=team_colors[away_team][0], edgecolor='black', linewidth=1)
     plt.xlabel('Runs Scored', fontsize=14)
     plt.ylabel('Frequency', fontsize=14)
-    plt.title(f'Distribution of Runs Scored ({num_simulations} Simulations)\nActual Score: {away_team} {str(away_score)} - {home_team} {str(home_score)}', fontsize=16)
+    plt.title(f'Distribution of Runs Scored ({num_simulations} Simulations)\nActual Score: {away_team} {str(away_score)} - {home_team} {str(home_score)}\nDeserve-to-Win: {away_team} {str(away_win_percentage_str)}% - {home_team} {str(home_win_percentage_str)}%', fontsize=16, pad=12)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
     plt.legend(fontsize=12)
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
     # Save the plot to the "images" folder in the repository
     images_dir = "images"
     if not os.path.exists(images_dir):
         os.makedirs(images_dir)
-    plt.savefig(os.path.join(images_dir, f'{away_team}_{home_team}_{str(away_score)}-{str(home_score)}--{str(away_win_percentage_str)}-{str(home_win_percentage_str)}_rd.png'))
+    plt.savefig(os.path.join(images_dir, f'{away_team}_{home_team}_{str(away_score)}-{str(home_score)}--{str(away_win_percentage_str)}-{str(home_win_percentage_str)}_rd.png'), bbox_inches='tight')
     plt.close()
