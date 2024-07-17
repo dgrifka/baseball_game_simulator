4f126219a3465cd76e6228743a763552f7b4ff89
Derek Grifka <65031380+dgrifka@users.noreply.github.com>
2024-07-16 21:18:53 -0500

    Update constants.py
    add all star colors

--- constants.py
+++ constants.py
@@ -54,5 +54,7 @@ team_colors = {
     "Rays": ("#092C5C", "292"),
     "Rangers": ("#C0111F", "293"),
     "Blue Jays": ("#134A8E", "294"),
-    "Nationals": ("#AB0003", "295")
+    "Nationals": ("#AB0003", "295"),
+    "National League All-Stars": ("#87CEEB", "blue"),
+    "American League All-Stars": ("#FF7F50", "coral")
 }
