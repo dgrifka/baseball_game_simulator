e4cc92c3026a6f70a5adb570fba18916ee4e5661
Derek Grifka <65031380+dgrifka@users.noreply.github.com>
2024-08-23 12:40:45 -0500

    Update visualizations.py
    adjust

--- visualizations.py
+++ visualizations.py
@@ -181,7 +181,7 @@ def run_dist(num_simulations, home_runs_scored, away_runs_scored, home_team, awa
     
 def create_estimated_bases_table(df, away_team, home_team, away_score, home_score, away_win_percentage, home_win_percentage, images_dir):
     # Create a new figure and axis, ensuring it's clear of any previous content
-    fig, ax = plt.subplots(figsize=(11.5, 7))  # Reduced figure height
+    fig, ax = plt.subplots(figsize=(11, 7))  # Reduced figure height
     
     # Clear the axis completely
     ax.clear()
@@ -268,13 +268,13 @@ def create_estimated_bases_table(df, away_team, home_team, away_score, home_scor
         ax.patches[0].remove()
     
     # Add watermark above the table
-    fig.text(0.5, 1.1, 'Data: MLB    By: @mlb_simulator', fontsize=15, color='darkgray', ha='center', va='center')
+    fig.text(0.5, 1.11, 'Data: MLB    By: @mlb_simulator', fontsize=15, color='darkgray', ha='center', va='center')
     
     # Set combined title above the watermark, aligned to the left
     plt.title(f'Top 15 Estimated Bases\n'
               f'Actual Score: {away_team} {away_score} - {home_team} {home_score}\n'
               f'Deserve-to-Win %: {away_team} {away_win_percentage:.0f}% - {home_team} {home_win_percentage:.0f}%', 
-              fontsize=17, loc='left', y=1.16)
+              fontsize=17, loc='left', y=1.18)
     
     # Adjust layout and save
     plt.tight_layout()
