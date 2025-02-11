707e515a5d7b320b1d22b6443f7d35a9f5a617b2
Derek Grifka <65031380+dgrifka@users.noreply.github.com>
2025-02-11 12:39:35 -0600

    Update constants.py
    change season to 2025

--- Simulator/constants.py
+++ Simulator/constants.py
@@ -1,7 +1,7 @@
 ## We want to create variables for the MLB Stats API criteria when pulling game data
 base_url = 'https://statsapi.mlb.com/api/'
 league = 1 ## League (MLB = 1)
-season = 2024
+season = 2025
 endpoint = f'schedule?language=en&sportId={league}&season={season}'
 
 ## Team version
