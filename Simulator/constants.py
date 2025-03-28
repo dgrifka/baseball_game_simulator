6c8f07b91ab59fc14de0679eba55c55d8dbae536
Derek Grifka <65031380+dgrifka@users.noreply.github.com>
2025-03-27 19:06:03 -0500

    Update constants.py
    Changed MMP to Daikin

--- Simulator/constants.py
+++ Simulator/constants.py
@@ -15,7 +15,7 @@ game_ver = 'v1.1'
 venue_names = ['Oriole Park at Camden Yards', 'Great American Ball Park',
        'Petco Park', 'Dodger Stadium', 'Tropicana Field',
        'Kauffman Stadium', 'Guaranteed Rate Field', 'loanDepot park',
-       'Minute Maid Park', 'Globe Life Field', 'Oakland Coliseum',
+       'Daikin Park', 'Globe Life Field', 'Oakland Coliseum',
        'Chase Field', 'T-Mobile Park', 'Citi Field', 'Citizens Bank Park',
        'Wrigley Field', 'Nationals Park', 'American Family Field',
        'Target Field', 'Busch Stadium', 'Yankee Stadium', 'Comerica Park',
